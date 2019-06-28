18a5bbe9902c0317da3d1b36a5afeb7bcebed99c
Moe <muhammad.2712@outlook.com>
2019-06-28 18:07:30 +0100

    Updated build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 
 cd $(dirname $0)
 build_date=$(date +%Y%m%d-%H%M%S)
-docker build -t localhost:5000/jenkins-docker --build-arg "BUILD_DATE=$build_date" .
-
+docker build -t mjah/jenkins-docker --build-arg "BUILD_DATE=$build_date" .
